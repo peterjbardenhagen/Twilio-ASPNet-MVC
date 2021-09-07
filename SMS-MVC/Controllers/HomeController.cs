527fa06a09ff849199139244c4463ee0cde41766
peter <peter@PETER>
2021-09-07 10:18:29 +1000

    Add project files.

---
+++ SMS-MVC/Controllers/HomeController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace SMS_MVC.Controllers
+{
+	public class HomeController : Controller
+	{
+		public ActionResult Index()
+		{
+			ViewBag.Message = "Index";
+			return View();
+		}
+
+		public ActionResult PostStatus()
+		{
+			try {
+				// Log the date created, message id and status
+				var dateCreated = DateTime.Now.ToString();
+				var smsSid = Request["MessageSid"];
+                var messageStatus = Request["MessageStatus"];
+                var logMessage = $"\"{dateCreated}\", \"{smsSid}\", \"{messageStatus}\"";
+
+                // Write the text to a a file named "log.txt".
+                System.IO.File.AppendAllText(Path.Combine(@"c:\logs", "log.txt"), logMessage + Environment.NewLine);
+				ViewBag.Message = logMessage;
+			}
+			catch (Exception ex)
+			{
+				try
+				{
+					// Display error
+					ViewBag.Message = "Message sending failed:" + ex.ToString();
+
+					// Write the error to a a file named "log.txt".
+					System.IO.File.AppendAllText(Path.Combine(@"c:\logs", "error.txt"), ex.ToString());
+				}
+				catch { }
+			}
+			return Content(ViewBag.Message);
+		}
+		
+		public ActionResult Status()
+		{
+			var fileContents = System.IO.File.ReadLines(@"c:\logs\log.txt").Last();
+			ViewBag.Message = "The latest update is " + fileContents;
+			return View();
+		}
+
+		public ActionResult GetStatus()
+		{
+			var fileContents = System.IO.File.ReadLines(@"c:\logs\log.txt").Last();
+			var message = "The latest update is " + fileContents;
+			return Content(message);
+		}
+	}
+}
---
+++ SMS-MVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SMS_MVC.MvcApplication" Language="C#" %>
